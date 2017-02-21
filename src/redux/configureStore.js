624a76d2126a2ee3a00f2f57771a91a0468a1db2
Jimmy Sinn <jimmyltsinn@gmail.com>
2017-02-21 19:11:31 +0800

    Use local storage to store chart config and restore in later session

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,4 +1,4 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import createLogger from 'redux-logger';
 
@@ -7,16 +7,23 @@ import reducer from './reducers';
 import {resizeWindow} from './actions/ui';
 import {fetchSolarTermMap} from './actions/data';
 
-import {fetchDataNeeded} from './actions/data';
+import {persistStore, autoRehydrate} from 'redux-persist'; 
 
 const configureStore = () => {
   let store = createStore(
     reducer,
-    applyMiddleware(
-      thunkMiddleware,
-      createLogger()
+    compose(
+      applyMiddleware(
+        thunkMiddleware,
+        createLogger()
+      ),
+      autoRehydrate()
     )
   );
+
+  persistStore(store, {
+    whitelist: ['chart']
+  });
 
   store.dispatch(resizeWindow(window.innerWidth, window.innerHeight));
   store.dispatch(fetchSolarTermMap());
